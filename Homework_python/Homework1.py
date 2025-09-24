86ca0fdca47c43824e1d7e8e4549f726fe1bcaab
Mattew552 <msemikov75@gmail.com>
2025-09-25 00:16:04 +0300

    Update Homework1.py

--- Homework_python/Homework1.py
+++ Homework_python/Homework1.py
@@ -1,7 +1,7 @@
-container=[1, 8, 1000, 2, 5, 4, 8, 3, 8]
+container=[1, 8, 6, 2, 5, 4, 8, 3, 7] #список стенок
 left=0
-right=len(container)-1
-maxS=0
+right=len(container)-1 #принцип 2 указателей, идущих навстречу друг другу
+maxS=0 #максимальная площадь
 while left<right:
     h=min(container[left], container[right])
     l=right-left
