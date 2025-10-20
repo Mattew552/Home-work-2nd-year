0cd812162f5b26d5a2ed984d7d01629f6a23b3c4
Mattew552 <msemikov75@gmail.com>
2025-10-20 09:04:35 +0300

    Create hwBD2.sql

---
+++ HomeworkBD/hwBD2.sql
@@ -0,0 +1,24 @@
+/* слияние
+select "GoodCategory".id, "GoodCategory".name from "GoodCategory" left outer join "Good" on
+("GoodCategory".id="Good"."id_goodCategory") left outer join "Good_Shop" on
+("Good".id="Good_Shop"."id_good")
+where ("Good_Shop".count=0) */
+
+
+/* in/not in
+select "GoodCategory".id, "GoodCategory".name from "GoodCategory" where
+"GoodCategory".id in (select "Good"."id_goodCategory" from "Good" where "Good".id in(
+select "Good_Shop".id_good from "Good_Shop" where "Good_Shop".count=0))
+*/
+
+
+/* all/any
+select "GoodCategory".id, "GoodCategory".name from "GoodCategory" where
+"GoodCategory".id =any (select "Good"."id_goodCategory" from "Good" where "Good".id =any(
+select "Good_Shop".id_good from "Good_Shop" where "Good_Shop".count=0))
+*/
+/* exists/not exists
+select id, name from "GoodCategory" gc where
+not exists(select * from "Good" g join "Good_Shop" gs on g.id=gs.id_good
+where g."id_goodCategory"=gc.id and gs.count is not null)
+*/
