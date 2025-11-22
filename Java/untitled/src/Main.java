68e1cbb96d8cdfa158b6c13f2f312211ea3509bb
Mattew552 <msemikov75@gmail.com>
2025-11-23 01:23:37 +0300

    Еще пара функций

---
+++ Java/untitled/src/Main.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+
+public class Main{
+    public static void main(String[] args){
+        Integer[] arr={3,2,-1,6,5,4};
+        Fenwick_tree<Integer> ft =new Fenwick_tree<>();
+        ft.build(arr);
+        System.out.print("Built tree"+ Arrays.toString(arr)+"\n");
+
+        System.out.print("Префиксная сумма первых 4 элементов массива "+ft.prefixSum(3)+" \n");
+        System.out.print("Префиксная сумма в некотором диапазоне "+ft.rangeSum(2,4)+" \n");
+        System.out.print(ft.prefixSum(0)+"\n");
+        ft.update(3,2);
+        System.out.print(ft.getValue(3)+"\n");
+        System.out.print("Тест функции суммы в обновленном массиве "+ft.rangeSum(2,4)+"\n");
+        ft.setValue(1,7);
+        System.out.print("Обновленное значение "+ ft.getValue(1)+"\n");
+        System.out.print("Префиксная сумма первых 2 элементов массива(обновленного) "+ft.prefixSum(1)+" \n");
+        System.out.print("Префиксная сумма первых 4 элементов массива(обновленного) "+ft.prefixSum(3)+" \n");
+
+
+    }
+}
